8990511b91f085ed017b5b125c4a82bc5cac6fa9
arizaga <arizaga@192.168.0.15>
2018-03-15 17:56:38 -0600

    Added bean class to comunicate

---
+++ inventory/src/com/neox/inventory/ws/bean/AreaBean.java
@@ -0,0 +1,26 @@
+package com.neox.inventory.ws.bean;
+
+public class AreaBean {
+	
+	private Integer idArea;
+	private Integer idCategory;
+	
+	public Integer getIdArea() {
+		return idArea;
+	}
+
+	public void setIdArea(Integer idArea) {
+		this.idArea = idArea;
+	}
+
+	public Integer getIdCategory() {
+		return idCategory;
+	}
+
+	public void setIdCategory(Integer idCategory) {
+		this.idCategory = idCategory;
+	}
+
+	public AreaBean() {}
+	
+}
